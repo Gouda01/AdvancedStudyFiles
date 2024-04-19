ad528442611d220625aca423e70b04a4f5ec6769
Mohamed Gouda <goda61@gmail.com>
2024-04-19 17:17:05 +0200

    Session 2 - Start 03-Data Classes - example 01

---
+++ 03-Data Classes/01.py
@@ -0,0 +1,17 @@
+# No data classes :
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return (f"Name: {self.name}, Age : {self.age}")
+        
+
+p1 = Person('Mohamed',20)
+
+print(p1.name)
+print(p1.age)
+print(p1)
+    

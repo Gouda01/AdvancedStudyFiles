153929b072fb60c600c520d62d1ef6803cfcbfa1
Mohamed Gouda <goda61@gmail.com>
2024-05-03 00:23:22 +0300

    Type Hints  - example 02

--- 04-Type Hints/01.py
+++ 04-Type Hints/01.py
@@ -1,23 +1,17 @@
 # We can define if integer or string
 
-# x:int = 10
+x:int = 10
 
 
-# def mysum(x:int, y:int) -> int:             # Return with int
-#     print(x+y)
+def mysum(x:int, y:int) -> int:             # Return with int
+    print(x+y)
 
 
-# def mysum(x:int, y:int) -> list[str]:       # Return with List
-#     print(x+y)
+def mysum(x:int, y:int) -> list[str]:       # Return with List
+    print(x+y)
 
-# def mysum(x:int, y:int) -> None:            # Return with None
-#     print(x+y)
+def mysum(x:int, y:int) -> None:            # Return with None
+    print(x+y)
 
 
 
-class Calc :
-    def sum(self, x, y) :
-        return x + y
-    
-    def mul(self, x, y) :
-        return x * y

bace433c6460f22df218a55e9bd2a2add8e25fe7
Mohamed Gouda <goda61@gmail.com>
2024-05-02 23:57:26 +0300

    Type Hints  - 01 - Return with none

--- 04-Type Hints/01.py
+++ 04-Type Hints/01.py
@@ -3,9 +3,12 @@
 x:int = 10
 
 
-def mysum(x:int, y:int) -> int:
+def mysum(x:int, y:int) -> int:             # Return with int
     print(x+y)
 
 
-def mysum(x:int, y:int) -> list[str]:
+def mysum(x:int, y:int) -> list[str]:       # Return with List
+    print(x+y)
+
+def mysum(x:int, y:int) -> None:            # Return with None
     print(x+y)
